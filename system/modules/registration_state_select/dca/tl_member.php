c817ac868d698cf722aed16570b2e31c209dec52
Andrew Stevens <andrew@andrewstevens.consulting>
2022-10-27 10:22:50 -0400

    Update tl_member.php

--- system/modules/registration_state_select/dca/tl_member.php
+++ system/modules/registration_state_select/dca/tl_member.php
@@ -4,7 +4,7 @@
  * Registration State Select
  * States and Canadian Provinces as a Select menu for member registration extension for Contao CMS
  *
- * Copyright (C) 2019 Andrew Stevens Consulting
+ * Copyright (C) 2019-2022 Andrew Stevens Consulting
  *
  * @package    asconsulting/registration_state_select
  * @link       https://andrewstevens.consulting
